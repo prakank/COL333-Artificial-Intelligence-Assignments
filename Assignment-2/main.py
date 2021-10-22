788370bfb2b13174907b433c790fad0a2f8a9b25
Prakhar Aggarwal <cs5190441@iitd.ac.in>
2021-10-23 00:57:16 +0530

    rosterg for less than 7 days

--- Assignment-2/main.py
+++ Assignment-2/main.py
@@ -19,9 +19,10 @@ class Node:
     def __init__(self, row, col):
         self.row = row
         self.col = col
-        self.out_nodes = np.array([])
-        self.in_nodes = np.array([])
+        self.out_nodes = []
+        self.in_nodes = []
         self.value = ""
+        
     def add_out(self, out_node):
         np.append(self.out_nodes, out_node)
 
@@ -35,13 +36,18 @@ class Node:
         return (self.row == node.row and self.col == node.col)
 
 def initialize_param(N, D):
-    Dx = np.array([np.array([np.array(["M", "A", "E", "R"]) for _ in range(D)]) for __ in range(N)])
+    # Dx = np.array([np.array([np.array(["M", "A", "E", "R"]) for _ in range(D)]) for __ in range(N)])
+    Dx = np.array([np.array([np.array(["A", "M", "E", "R"]) for _ in range(D)]) for __ in range(N)])
     X = np.array([np.array([None for _ in range(D)]) for __ in range(N)])
     for r in range(N):
         for c in range(D):
             X[r][c] = Node(r,c)
-            if (r >= 1): X[r][c].add_in(X[r-1][c])
-            if (c >= 1): X[r][c].add_in(X[r][c-1])
+            if (r >= 1):
+                X[r][c].in_nodes.append(X[r-1][c])
+                X[r-1][c].out_nodes.append(X[r][c])
+            if (c >= 1):
+                X[r][c].in_nodes.append(X[r][c-1])
+                X[r][c-1].out_nodes.append(X[r][c])
     return Dx, X
 
 def arc_valid(X, Xi, Xj, param):
@@ -54,16 +60,15 @@ def arc_valid(X, Xi, Xj, param):
             return False
         if ((Xj.col+1) % 7 == 0):
             for dist in range(7):
-                if (X[Xj.row][Xj.col-dist] == "R"):
+                if (X[Xj.row][Xj.col-dist].value == "R"):
                     return True
             return False
         return True
     else:
-        count = {
-                "m" : 0, "a" : 0, "e" : 0
-            }
+        count = {"m" : 0, "a" : 0, "e" : 0, "r" : 0}
         for r in range(Xj.row+1):
-            count[X[r][Xj.col].value] += 1
+            count[(X[r][Xj.col].value).lower()] += 1
+            
         if (Xj.row == param["N"]-1):
             '''Xj in last row'''
             if (count["m"] == param["m"] and count["a"] == param["a"] and count["e"] == param["e"]):
@@ -73,50 +78,78 @@ def arc_valid(X, Xi, Xj, param):
             if (count["m"] > param["m"] or count["a"] > param["a"] or count["e"] > param["e"]):
                 return False
             return True
-def revise(Xi, Xj, Dx):
+        
+def revise(X, Xi, Xj, Dx, param):
     revised = False
     for x_dom in Dx[Xi.row][Xi.col]:
+        if x_dom == '.':
+            continue
         satisfied = False
+        
         for y_dom in Dx[Xj.row][Xj.col]:
+            if y_dom == '.':
+                continue            
             (xi_init, xj_init) = (Xi.value, Xj.value)
             Xi.value = x_dom
             Xj.value = y_dom
-            if (arc_valid(Xi, Xj)):
+            if (arc_valid(X, Xi, Xj, param)):
                 satisfied = True
             Xi.value = xi_init
             Xj.value = xj_init
-        if not satisfied:
-            Dx[Xi.row][Xj.row] = np.delete(Dx[Xi.row][Xj.row], np.argwhere(Dx[Xi.row][Xj.row] == x_dom))
+        
+        if not satisfied:            
+            index = np.argwhere(Dx[Xi.row][Xi.col] == x_dom)[0][0]
+            Dx[Xi.row][Xi.col][index] = '.'            
             revised = True
+            
     return revised, Dx
 
-def AC_3(arcs, Dx):
+def AC_3(X, arcs, Dx, param):
     '''
     arcs <= Queue
     '''
     while(len(arcs) > 0):
         (Xi, Xj) = arcs.pop(0)
-        revised, Dx = revise(Xi, Xj, Dx)
+        revised, Dx = revise(X, Xi, Xj, Dx, param)
         if (revised == True):
-            if (len(Dx[Xi.row][Xi.col]) == 0):
+            if (np.count_nonzero(Dx[Xi.row][Xi.col] == '.') == 4):
                 return (False, Dx)
             for in_nbr in Xi.in_nodes:
                 if not in_nbr.is_equal(Xj):
                     arcs.append((in_nbr, Xi))
     return (True, Dx)
 
-def back_track(row, col, param, Dx, X):
-    if (row == param["N"] or col == param["D"]):
-        return X
-    new_r, new_c = row, col
+count = 0
+def back_track(row, col, param, Dx, X):    
+    if (col == param["D"]):
+        row = row+1
+        col = 0    
+    if row >= param["N"] or col >= param["D"]:
+        return X    
     curr_node = X[row][col]
-    if (col == param["D"]-1):
-        new_r = row+1
-        new_c = 0
-    else:
-        new_c += 1
+    
+    global count
+    print("Count: {}, Row: {}, Col: {}".format(count, row, col))
+    count+=1
+    
+    # if col > 5:
+    #     print("Start: Row:{}, Col:{}".format(row,col))
+    #     for i in range(row+1):
+    #         p=False
+    #         for j in range(col+1):
+    #             if i==row and j==col:
+    #                 p = True
+    #                 break
+    #             print(X[i][j].value,end=" ")        
+    #         print("")
+    #         if p:
+    #             break
+    
     for x_dom in Dx[row][col]:
+        if x_dom == '.':
+            continue        
         consistent = True
+        
         for in_node in curr_node.in_nodes:
             curr_node.assign_node(x_dom)
             if not arc_valid(X, in_node, curr_node, param):
@@ -128,10 +161,9 @@ def back_track(row, col, param, Dx, X):
             for in_node in curr_node.in_nodes:
                 arcs.append((in_node, curr_node))
             Dx_new = np.copy(Dx)
-            ac_val, Dx_updated = AC_3(arcs, Dx_new)
+            ac_val, Dx_updated = AC_3(X, arcs, Dx_new, param)
             if (ac_val):
-                result = back_track(new_r, new_c, param, Dx_updated, X)
-                #print(result)
+                result = back_track(row, col+1, param, Dx_updated, X)
                 if (len(result) > 0):
                     return result
             curr_node.value = ""
@@ -139,23 +171,42 @@ def back_track(row, col, param, Dx, X):
 
 def solve_csp(input_path):
     param = read_input(input_path)
+    print(param)
+    if param["m"] + param["a"] + param["e"] > param["N"]:
+        print("Invalid arguments for m, a, e, N")
+        print( "(m + a + e <= N) should hold")
+        sys.exit(1)
+    
+    if param["D"] >= 7 and param["m"] + param["a"] + param["e"] == param["N"]:
+        print("Invalid arguments for m, a, e, N")
+        print( "(m + a + e < N) should hold {Strictly less cause each nurse needs to have atleast 1 R shift in a week}")
+        sys.exit(1)
+            
     Dx, X = initialize_param(param["N"], param["D"])
     return back_track(0, 0, param, Dx, X)
 
 def get_result(X):
-    if (X == []):
+    if len(X) == 0:
         ans = dict()
     else:
         N = len(X)
         M = len(X[0])
         ans = dict()
         for i in range(N):
+            print("N-"+str(i)+": ",end="")
             for j in range(M):
+                print(X[i][j].value + " ",end=" ")
                 ans["N{}_{}".format(str(i),str(j))] = X[i][j].value
+            print()
+            
     with open("solution.json" , 'w') as file:
         json.dump(ans,file)
         file.write("\n")
+        
 if __name__ == "__main__":
     input_path = sys.argv[1]
     result = solve_csp(input_path)
     get_result(result)
+    
+
+# Look out for these settings
