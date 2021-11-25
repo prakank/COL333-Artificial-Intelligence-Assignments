edbe99eade7acc10e2563ade9510c71b58f84e05
Prakhar Aggarwal <cs5190441@iitd.ac.in>
2021-11-25 19:05:31 +0530

    generalized MDP

--- Assignment-3/a.py
+++ Assignment-3/a.py
@@ -47,23 +47,19 @@ class MarkovDecisionProblem:
                         self.transitions[action][0]["p"] += self.transitions[action][i]["p"]
                         self.transitions[action][i]["p"] = 0
 
-    def __init__(self, discount, epsilon, success_prob, gridType):
+    def __init__(self, discount, epsilon, success_prob, gridType, params):
         self.possibleActions = ["N", "S", "W", "E", "U", "D"]
         self.discount = discount
         self.epsilon = epsilon
         self.discountedEpsilon = epsilon*(1-discount)/discount
         self.success_prob = success_prob
         self.passengerPicked = False
-
-        if gridType == 'easy':
-            self.generate_easy()
-        elif gridType == 'hard':
-            self.generate_hard()
-
-    def generate_easy(self):
-        # 5*5 matrix
-        self.rows = rows = 5
-        self.cols = cols = 5
+        self.gridType = gridType        
+        self.generate(params)
+    
+    def generate(self, params):
+        self.rows = rows = params['rows']
+        self.cols = cols = params['cols']
         grid = [[[[[None for i1 in range(cols)]
                  for j1 in range(rows)] for i2 in range(cols)] for j2 in range(rows)]for k in range(2)]
 
@@ -84,192 +80,195 @@ class MarkovDecisionProblem:
                             node = self.Node(ps, tr, tc, cr, cc)
                             if tr == 0:
                                 node.change_weights(tr-1, tc)
-                            if tr == 4:
+                            if tr == (rows-1):
                                 node.change_weights(tr+1, tc)
                             if tc == 0:
                                 node.change_weights(tr, tc-1)
-                            if tc == 4:
+                            if tc == (cols-1):
                                 node.change_weights(tr, tc+1)
                             grid[ps][tr][tc][cr][cc] = node
-
-        for ps in range(2):
-            for cr in range(rows):
-                for cc in range(cols):
-                    grid[ps][0][1][cr][cc].change_weights(0, 2)
-                    grid[ps][0][2][cr][cc].change_weights(0, 1)
-
-                    grid[ps][1][1][cr][cc].change_weights(1, 2)
-                    grid[ps][1][2][cr][cc].change_weights(1, 1)
-
-                    grid[ps][3][0][cr][cc].change_weights(3, 1)
-                    grid[ps][3][1][cr][cc].change_weights(3, 0)
-
-                    grid[ps][3][2][cr][cc].change_weights(3, 3)
-                    grid[ps][3][3][cr][cc].change_weights(3, 2)
-
-                    grid[ps][4][0][cr][cc].change_weights(4, 1)
-                    grid[ps][4][1][cr][cc].change_weights(4, 0)
-
-                    grid[ps][4][2][cr][cc].change_weights(4, 3)
-                    grid[ps][4][3][cr][cc].change_weights(4, 2)
-
+        self.grid = grid
+        
+        if self.gridType == 'easy':
+            self.generate_easy()
+        else:
+            self.generate_hard()
+        
         for tr in range(rows):
             for tc in range(cols):
                 for cr in range(rows):
                     for cc in range(cols):
                         if tr != cr or tc != cc:
-                            grid[1][tr][tc][cr][cc] = None
+                            self.grid[1][tr][tc][cr][cc] = None
+        
+        if 'passenger' in params:            
+            self.passenger = params['passenger']
+            self.dest = params['dest']
+            self.taxi = params['taxi']
+        else:
+            passenger = random.randrange(1, 5)
+            dest = passenger
+            while dest == passenger:
+                dest = random.randrange(1, 5)
 
-        self.grid = grid
+            taxi = (random.randrange(5), random.randrange(5))
 
-        self.Rloc = (0, 0)
-        self.Gloc = (0, 4)
-        self.Yloc = (4, 0)
-        self.Bloc = (4, 3)
+            self.taxi = taxi
 
-        # self.taxi = self.Node(random.randrange(0, 5), random.randrange(0, 5))
+            def depot(x):
+                if x == 1:
+                    loc = self.Rloc
+                elif x == 2:
+                    loc = self.Gloc
+                elif x == 3:
+                    loc = self.Yloc
+                else:
+                    loc = self.Bloc
+                return loc
 
-        # 2 points for client out of 4 depots cr,cc start.     dr,dc end
-        # 1 point for taxi in 5x5 tr,tc start
-        # initial state: 0,tr,tc,cr,cc
-
-        # 1-R, 2-G, 3-Y, 4-B
-        passenger = random.randrange(1, 5)
-        dest = passenger
-        while dest == passenger:
-            dest = random.randrange(1, 5)
-
-        taxi = (random.randrange(5), random.randrange(5))
-
-        self.taxi = taxi
-
-        def depot(x):
-            if x == 1:
-                loc = (0, 0)
-            elif x == 2:
-                loc = (0, 4)
-            elif x == 3:
-                loc = (4, 0)
-            else:
-                loc = (4, 3)
-            return loc
-
-        self.passenger = depot(passenger)
-        self.dest = depot(dest)
-        # self.grid[0][self.dest[0]][self.dest[1]
-        #                            ][self.dest[0]][self.dest[1]].transitions = {}
-        # self.grid[0][self.dest[0]][self.dest[1]][self.dest[0]][self.dest[1]]
+            self.passenger = depot(passenger)
+            self.dest = depot(dest)
+        
         self.grid[1][self.dest[0]][self.dest[1]][self.dest[0]
                                                  ][self.dest[1]].transitions["D"][0]["r"] = 20
         self.grid[0][self.dest[0]][self.dest[1]][self.dest[0]
                                                  ][self.dest[1]].transitions = {}
+                            
+    def generate_easy(self):        
+        # Walls
+        rows, cols = self.rows, self.cols
+        
+        for ps in range(2):
+            for cr in range(rows):
+                for cc in range(cols):
+                    self.grid[ps][0][1][cr][cc].change_weights(0, 2)
+                    self.grid[ps][0][2][cr][cc].change_weights(0, 1)
 
-    def utilityValue(self, ps, tr, tc, cr, cc):
-        val = -float('inf')
-        bestAction = -1
+                    self.grid[ps][1][1][cr][cc].change_weights(1, 2)
+                    self.grid[ps][1][2][cr][cc].change_weights(1, 1)
 
-        for i in self.grid[ps][tr][tc][cr][cc].transitions:  # Number of actions
-            temp = self.Q_Value(ps, tr, tc, cr, cc, i)
-            if temp > val:
-                val = temp
-                bestAction = i
+                    self.grid[ps][3][0][cr][cc].change_weights(3, 1)
+                    self.grid[ps][3][1][cr][cc].change_weights(3, 0)
 
-        return val, bestAction
+                    self.grid[ps][3][2][cr][cc].change_weights(3, 3)
+                    self.grid[ps][3][3][cr][cc].change_weights(3, 2)
 
-    def Q_Value(self, ps, tr, tc, cr, cc, action):
-        node = self.grid[ps][tr][tc][cr][cc]
-        q = 0
-        for trans in node.transitions[action]:
-            nps, ntr, ntc, ncr, ncc = trans["state"]
-            if trans["p"] != 0:
-                # print(trans)
-                q += (trans["p"] * (trans["r"] + self.discount *
-                      self.V[nps][ntr][ntc][ncr][ncc]))
-        return q
+                    self.grid[ps][4][0][cr][cc].change_weights(4, 1)
+                    self.grid[ps][4][1][cr][cc].change_weights(4, 0)
 
-    def value_iteration(self):
-        iteration = 0
+                    self.grid[ps][4][2][cr][cc].change_weights(4, 3)
+                    self.grid[ps][4][3][cr][cc].change_weights(4, 2)
+        
+        self.Rloc = (0, 0)
+        self.Gloc = (0, 4)
+        self.Yloc = (4, 0)
+        self.Bloc = (4, 3)
+        
 
-        while True:
-            iteration += 1
-            delta = 0.0
-            for ps in range(2):
-                for tr in range(self.rows):
-                    for tc in range(self.cols):
-                        for cr in range(self.rows):
-                            for cc in range(self.cols):
-                                if not self.grid[ps][tr][tc][cr][cc]:
-                                    continue
-                                if self.grid[ps][tr][tc][cr][cc].transitions == {}:
-                                    continue
-                                self.V_temp[ps][tr][tc][cr][cc], action = self.utilityValue(
-                                    ps, tr, tc, cr, cc)
-                                self.policy[ps][tr][tc][cr][cc] = action
+def utilityValue(MDP, ps, tr, tc, cr, cc):
+    val = -float('inf')
+    bestAction = -1
 
-                                delta = max(delta, abs(
-                                    self.V_temp[ps][tr][tc][cr][cc] - self.V[ps][tr][tc][cr][cc]))
+    for i in MDP.grid[ps][tr][tc][cr][cc].transitions:  # Number of actions
+        temp = Q_Value(MDP,ps, tr, tc, cr, cc, i)
+        if temp > val:
+            val = temp
+            bestAction = i
 
-            self.V = copy.deepcopy(self.V_temp)
+    return val, bestAction
 
-            print("Iteration:{}, Delta:{}".format(iteration, delta))
+def Q_Value(MDP, ps, tr, tc, cr, cc, action):
+    node = MDP.grid[ps][tr][tc][cr][cc]
+    q = 0
+    for trans in node.transitions[action]:
+        nps, ntr, ntc, ncr, ncc = trans["state"]
+        if trans["p"] != 0:
+            q += (trans["p"] * (trans["r"] + MDP.discount *
+                    MDP.V[nps][ntr][ntc][ncr][ncc]))
+    return q
 
-            if delta <= self.discountedEpsilon:
-                break
+def value_iteration(MDP):
+    iteration = 0
 
-        print("\nConverged\nIterations:{}, Max-Norm:{}".format(iteration, delta))
-        print("Passenger:({},{})".format(
-            self.passenger[0], self.passenger[1]))
-        print("Destination:({},{})".format(self.dest[0], self.dest[1]))
-        print("Taxi:({},{})".format(self.taxi[0], self.taxi[1]))
+    while True:
+        iteration += 1
+        delta = 0.0
+        for ps in range(2):
+            for tr in range(MDP.rows):
+                for tc in range(MDP.cols):
+                    for cr in range(MDP.rows):
+                        for cc in range(MDP.cols):
+                            if not MDP.grid[ps][tr][tc][cr][cc]:
+                                continue
+                            if MDP.grid[ps][tr][tc][cr][cc].transitions == {}:
+                                continue
+                            MDP.V_temp[ps][tr][tc][cr][cc], action = utilityValue(MDP,ps, tr, tc, cr, cc)
+                            MDP.policy[ps][tr][tc][cr][cc] = action
 
-        tr, tc = self.taxi[0], self.taxi[1]
-        cr, cc = self.passenger[0], self.passenger[1]
+                            delta = max(delta, abs(
+                                MDP.V_temp[ps][tr][tc][cr][cc] - MDP.V[ps][tr][tc][cr][cc]))
 
-        action = ''
-        picked = False
+        MDP.V = copy.deepcopy(MDP.V_temp)
 
-        while not (tr == self.dest[0] and tc == self.dest[1] and picked == False and cr == tr and cc == tc):
-            if not picked:
-                action = self.policy[0][tr][tc][cr][cc]
-            else:
-                action = self.policy[1][tr][tc][tr][tc]
-            ret = self.simulate(picked, tr, tc, cr, cc, action)
-            (picked, tr, tc, cr, cc) = ret["state"]
-            reward = ret["r"]
-            print(action, ret)
+        print("Iteration:{}, Delta:{}".format(iteration, delta))
 
-            # for r in range(self.rows):
-            #     for c in range(self.cols):
-            #         # print(round(self.utility[r][c], 3), end=" ")
-            #         action = self.policy[r][c]
-            #         direction = ""
-            #         if action == 0:
-            #             direction = "↑"
-            #         elif action == 1:
-            #             direction = "→"
-            #         elif action == 2:
-            #             direction = "↓"
-            #         elif action == 3:
-            #             direction = "←"
-            #         elif action == 4:
-            #             direction = "Pick"
-            #         elif action == 5:
-            #             direction = "Putdown"
-            #         else:
-            #             direction = "Error"
-            #         print(direction, end=" ")
+        if delta <= MDP.discountedEpsilon:
+            break
 
-    def simulate(self, ps, tr, tc, cr, cc, action):
-        r = random.random()
-        # print(r)
-        for trans in self.grid[ps][tr][tc][cr][cc].transitions[action]:
-            if trans["p"] == 0:
-                continue
-            else:
-                r -= trans["p"]
-            if r < 0:
-                return trans
-        return None
+    print("\nConverged\nIterations:{}, Max-Norm:{}".format(iteration, delta))
+    print("Passenger:({},{})".format(
+        MDP.passenger[0], MDP.passenger[1]))
+    print("Destination:({},{})".format(MDP.dest[0], MDP.dest[1]))
+    print("Taxi:({},{})".format(MDP.taxi[0], MDP.taxi[1]))
+
+    tr, tc = MDP.taxi[0], MDP.taxi[1]
+    cr, cc = MDP.passenger[0], MDP.passenger[1]
+
+    action = ''
+    picked = False
+
+    while not (tr == MDP.dest[0] and tc == MDP.dest[1] and picked == False and cr == tr and cc == tc):
+        if not picked:
+            action = MDP.policy[0][tr][tc][cr][cc]
+        else:
+            action = MDP.policy[1][tr][tc][tr][tc]
+        ret = simulate(MDP,picked, tr, tc, cr, cc, action)
+        (picked, tr, tc, cr, cc) = ret["state"]
+        reward = ret["r"]
+        print(action, ret)
+
+        # for r in range(self.rows):
+        #     for c in range(self.cols):
+        #         # print(round(self.utility[r][c], 3), end=" ")
+        #         action = self.policy[r][c]
+        #         direction = ""
+        #         if action == 0:
+        #             direction = "↑"
+        #         elif action == 1:
+        #             direction = "→"
+        #         elif action == 2:
+        #             direction = "↓"
+        #         elif action == 3:
+        #             direction = "←"
+        #         elif action == 4:
+        #             direction = "Pick"
+        #         elif action == 5:
+        #             direction = "Putdown"
+        #         else:
+        #             direction = "Error"
+        #         print(direction, end=" ")
+
+def simulate(MDP, ps, tr, tc, cr, cc, action):
+    r = random.random()
+    # print(r)
+    for trans in MDP.grid[ps][tr][tc][cr][cc].transitions[action]:
+        if trans["p"] == 0:
+            continue
+        else:
+            r -= trans["p"]
+        if r < 0:
+            return trans
+    return None
 
 
 def q_learning(MDP, episodes, learning_rate, epsilon_exploration=0.1, decay=False):
@@ -305,5 +304,14 @@ def q_learning(MDP, episodes, learning_rate, epsilon_exploration=0.1, decay=Fals
 
 
 if __name__ == '__main__':
-    MDP = MarkovDecisionProblem(0.9, 1e-6, 0.85, 'easy')
-    MDP.value_iteration()
+    
+    params = {
+        'rows': 5,
+        'cols': 5,
+        'passenger': (4,3),
+        'dest': (4,0),
+        'taxi': (1,4)
+    }
+    
+    MDP = MarkovDecisionProblem(0.9, 1e-6, 0.85, 'easy', params=params)
+    value_iteration(MDP)
